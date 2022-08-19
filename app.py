fec5da5a267d035fc5d12847f0498f5d36409aa5
Ibran786 <111190109+Ibran786@users.noreply.github.com>
2022-08-20 01:58:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route('/')
+def a():
+    return render_template("calculator.html")
+@app.route('/calculator',methods=['POST'])
+def b():
+    if request.method=="POST":
+        x=float(request.form['f'])
+        y=float(request.form['s'])
+        z=request.form['o']
+        if z=="+":
+            result=x+y
+        elif z=="-":
+            result=x-y
+        elif z=="*":
+            result=x*y
+        elif z=="/":
+            result=x/y
+        else:
+            result="Incorrect Operator"
+    return render_template("calculator.html",result=result)
+
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
